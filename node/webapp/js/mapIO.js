3da3c7be7f65ac9a538f8ac2ec31bea0db5cfd8f
k1gva <k1.gva@wwu.de>
2015-07-13 19:23:04 +0200

    saveToDB() schickt jetzt GeoJSON...

--- node/webapp/js/mapIO.js
+++ node/webapp/js/mapIO.js
@@ -19,7 +19,7 @@ function saveToDB() {
      var name = prompt('wie solls denn heißen wenns fertig is?');
      var contentString = $('#drawnItemJSONText').val();
      
-     if ( name != undefined && contentString != '' ) {
+     if ( typeof data.waypoints !== undefined && waypoints != null ) {
 
           var content = JSON.parse( contentString );
           var url = $('#db-url').val() + '/addFeature?name=' + name;
@@ -95,12 +95,15 @@ function loadFromDB() {
  */
 function saveToDBRoute() {
     var name = prompt('Wie soll die Route heissen?');
-    var route = routeControl.getWaypoints();
     
-    console.log(route);
-    
-    if ( name != undefined && route != '' ) {
-        var content = route;
+    var content = JSON.parse(JSON.stringify(
+        { waypoints : routeControl.getWaypoints() }
+    ));
+        
+    if ( name != undefined && content != '' ) {
+        
+        console.log(content);
+        
         var url = $('#db-url').val() + '/addFeature?name=' + name;
 
         // perform post ajax
@@ -118,13 +121,55 @@ function saveToDBRoute() {
           });
 
           //refresh table
-          loadFromDB();
+          loadFromDBRoute();
      } else {
           JL('ajaxLogger').error('unable to save to database: no JSON or name provided');
      }
 };
 
+/**
+ * loads the contents from the in #db-url specified database server
+ * and inserts the loaded routes into the map
+ */
+function loadFromDBRoute() {
+     var url = $('#db-url').val() + '/getFeatures';
 
+     // perform ajax on the url and add the loaded GeoJSON
+     $.ajax({
+       type: 'GET',
+       dataType: 'json',
+       url: url,
+       timeout: 5000,
+       success: function(content, textStatus ){
+
+          JL('ajaxLogger').info('database content was retrieved from ' + url
+                              + ' (' + content.length + ' items)');
+
+          // remove existing items
+          $('#tableDBContentsRoute').empty();
+          dbFeatures.eachLayer(function(layer) {
+	           layerControl.removeLayer(layer);
+	      });
+
+          dbFeatures.clearLayers();
+
+          for (var i = 0; i < content.length; i++) {
+               // insert each received layer into the table in the html
+               $('#tableDBContentsRoute').append('<tr><td>'
+                    + (i+1) + '</td><td>' 
+                    + content[i].name + '</td><td>' 
+                    + content[i].data + '</td><td>' 
+                    + content[i].dateInserted + '</td></tr>');
+               // insert into the map
+               addLayerToGroup(L.geoJson(content[i].data), content[i].name + ' (database ' + (i+1) + ')', dbFeatures)
+          }
+          $('#tableDBRoute').removeClass('hidden');
+       },
+       error: function(xhr, textStatus, errorThrown){
+          JL('ajaxLogger').error("unable to get database content (" + errorThrown + ")");
+       }
+     });
+};
 
 /**  
 * @desc   loads a GeoJSON file from a given url in the textfield "urlTextfield"
